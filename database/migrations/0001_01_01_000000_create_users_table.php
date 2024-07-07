404abad0e1a15136bebbeebf940b1044f7c7e442
Hassaan-here <hassaanmuneer854@gmail.com>
2024-07-08 00:03:35 +0500

    Theme implemented

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('phone_no')->nullable();
+            $table->longText('address')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
